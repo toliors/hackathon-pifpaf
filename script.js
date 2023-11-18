6f2a2b43a5c15742d9816e5be7eed8b6ab0040bb
Frrz <72082595+Frr-z@users.noreply.github.com>
2023-11-18 16:44:13 +0000

    commit js

---
+++ script.js
@@ -0,0 +1,54 @@
+class Caminhao{
+    DestinoAves;
+    AvesPorVeiculo;
+    Produtor;
+    Galpao;
+    PesoPrevistoDasAves;
+    AvesPorCaixa;
+    PesoPrevistoPorCaixa;
+    Municipio;
+    Data;
+    HorarioDeRetiradaDaRacao;
+    Horario;
+    TurmaDeApanha;
+    Idade;
+    HorarioPrevistoChegadaFrigorifico;
+    HorarioPrevistoInicioDoAbate;
+    OrdemDoCaminhao;
+    Placa;
+    NotaFiscal;
+    HorarioDeChegada;
+
+    GetInfo(){
+        return {
+        "DestinoAves";
+        "AvesPorVeiculo";
+        "Produtor";
+        "Galpao";
+        "PesoPrevistoDasAves";
+        "AvesPorCaixa";
+        "PesoPrevistoPorCaixa";
+        "Municipio";
+        "Data";
+        "HorarioDeRetiradaDaRacao";
+        "Horario";
+        "TurmaDeApanha";
+        "Idade";
+        "HorarioPrevistoChegadaFrigorifico";
+        "HorarioPrevistoInicioDoAbate";
+        "OrdemDoCaminhao";
+        "Placa";
+        "NotaFiscal";
+        "HorarioDeChegada";
+        }
+    }
+}
+
+let ProdutoresAtivos = {
+    "a" : {},
+    "b" : {}
+};
+
+let Vagas = {};
+ 
+
