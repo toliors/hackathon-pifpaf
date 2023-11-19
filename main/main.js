a22d8aac7644205f123ab8f50c3cad8c127c4d61
Frrz <72082595+Frr-z@users.noreply.github.com>
2023-11-19 03:05:49 +0000

    dnuiawudwsnji

--- main/main.js
+++ main/main.js
@@ -3,6 +3,25 @@ const MainButton = document.getElementById("MainButton");
 const MessageButton = document.getElementById("message");
 var Botoes = document.getElementsByClassName("block");
 
+let CaminhoesAtivos = {};
+var Vagas = [
+    Interna= [
+        { "Ocupada": false },
+        { "Ocupada": false },
+        { "Ocupada": false },
+        { "Ocupada": false }
+    ],
+    Externa= [
+        { "Ocupada": false },
+        { "Ocupada": false },
+        { "Ocupada": false },
+        { "Ocupada": false },
+        { "Ocupada": false },
+        { "Ocupada": false }
+    ]
+];
+
+
 class Caminhao{
     DestinoAves;
     AvesPorVeiculo;
@@ -84,52 +103,15 @@ class Caminhao{
         Vagas.forEach(Tipo => {
             Tipo.forEach(Vaga =>{
                 if(Vaga.Ocupada == false && this.Vaga == null){
-                    Botoes[CaminhoesAtivos.lenght].innerText = this.Placa;
+               //     Botoes[CaminhoesAtivos.lenght].innerText = this.Placa;
                     this.Vaga = Vaga;
                     Vaga.Ocupada = true;
-                    CaminhoesAtivos[CaminhoesAtivos.lenght + 1] = this;
+                    CaminhoesAtivos[this.OrdemDoCaminhao] = this;
                 }
             })
          });   
     }
 }
-
-let CaminhoesAtivos = {};
-let Vagas = {
-    "Interna": {
-        "1": {
-            Ocupada: false
-        },
-        "2": {
-            Ocupada: false
-        },
-        "3": {
-            Ocupada: false
-        },
-        "4": {
-            Ocupada: false
-        }},
-    "Externa":{
-        "1":{
-            Ocupada: false 
-        },
-        "2": {
-            Ocupada: false 
-        },
-        "3": {
-            Ocupada: false 
-        },
-        "4": {
-            Ocupada: false 
-        },
-        "5": {
-            Ocupada: false 
-        },
-        "6": {
-            Ocupada: false
-        }
-    }
-};    
 
 function Remover(Placa, Justificativa){
     CaminhoesAtivos.forEach(Carga => {
@@ -195,8 +177,230 @@ function Setup(Placa){
     }
 }
 
-const Infos = jQuery.getJSON("./info.json", function(Data) {
-    jQuery.each(Data, function(Key, Value){
-        new Caminhao(Data[0], Data[1], Data[2], Data[3], Data[4], Data[5], Data[6], Data[7], Data[9], Data[10], Data[11], Data[12], Data[13], Data[14], Data[15], Data[16], Data[17], Data[18],Data[19]);
-    })
-});
+const Infos = [
+        {
+            "Destino": "SIF - 926 (FABRIL)",
+            "Aves por veículo": "4560",
+            "Produtor": "JOSE CELSO DA SILVA LESSA(POSITIVO)",
+            "Galpão": "1",
+            "Sexo": "FEMEA",
+            "Peso previsto das aves": "2800",
+            "Aves por caixa": "8",
+            "Peso previsto por caixa": "22400",
+            "Município": "Coimbra",
+            "Data": "15/11/2023",
+            "Horário de retirada da ração": "19:00",
+            "Horário": "22:00",
+            "Turma de apanha": "SINHORINHO",
+            "Idade": "47",
+            "Horário previsto chegada frigorífico": "23:40",
+            "Horário previsto início do abate": "03:30",
+            "Ordem dos caminhões p/ abate": "1",
+            "Placa": "XXXX-000",
+            "Nota fiscal": "FDIONU123",
+            "Horário de chegada": "23:40"
+        },
+        {
+            "Destino": "SIF - 926 (FABRIL)",
+            "Aves por veículo": "4560",
+            "Produtor": "JOSE CELSO DA SILVA LESSA(POSITIVO)",
+            "Galpão": "1",
+            "Sexo": "FEMEA",
+            "Peso previsto das aves": "2800",
+            "Aves por caixa": "8",
+            "Peso previsto por caixa": "22400",
+            "Município": "Coimbra",
+            "Data": "15/11/2023",
+            "Horário de retirada da ração": "19:00",
+            "Horário": "23:00",
+            "Turma de apanha": "SINHORINHO",
+            "Idade": "47",
+            "Horário previsto chegada frigorífico": "00:40",
+            "Horário previsto início do abate": "03:59",
+            "Ordem dos caminhões p/ abate": "2",
+            "Placa": "XXXX-000",
+            "Nota fiscal": "FDIONU123",
+            "Horário de chegada": "00:40"
+        },
+        {
+            "Destino": "SIF - 926 (FABRIL)",
+            "Aves por veículo": "4560",
+            "Produtor": "JOSE CELSO DA SILVA LESSA(POSITIVO)",
+            "Galpão": "1",
+            "Sexo": "FEMEA",
+            "Peso previsto das aves": "2800",
+            "Aves por caixa": "8",
+            "Peso previsto por caixa": "22400",
+            "Município": "Coimbra",
+            "Data": "16/11/2023",
+            "Horário de retirada da ração": "19:00",
+            "Horário": "00:00",
+            "Turma de apanha": "SINHORINHO",
+            "Idade": "47",
+            "Horário previsto chegada frigorífico": "01:40",
+            "Horário previsto início do abate": "04:46",
+            "Ordem dos caminhões p/ abate": "4",
+            "Placa": "XXXX-000",
+            "Nota fiscal": "FDIONU123",
+            "Horário de chegada": "01:40"
+        },
+        {
+            "Destino": "SIF - 926 (FABRIL)",
+            "Aves por veículo": "4560",
+            "Produtor": "JOSE CELSO DA SILVA LESSA(POSITIVO)",
+            "Galpão": "1",
+            "Sexo": "FEMEA",
+            "Peso previsto das aves": "2800",
+            "Aves por caixa": "8",
+            "Peso previsto por caixa": "22400",
+            "Município": "Coimbra",
+            "Data": "16/11/2023",
+            "Horário de retirada da ração": "19:00",
+            "Horário": "01:00",
+            "Turma de apanha": "SINHORINHO",
+            "Idade": "47",
+            "Horário previsto chegada frigorífico": "02:40",
+            "Horário previsto início do abate": "05:53",
+            "Ordem dos caminhões p/ abate": "6",
+            "Placa": "XXXX-000",
+            "Nota fiscal": "FDIONU123",
+            "Horário de chegada": "02:40"
+        },
+        {
+            "Destino": "SIF - 926 (FABRIL)",
+            "Aves por veículo": "3171",
+            "Produtor": "JOSE CELSO DA SILVA LESSA(POSITIVO)",
+            "Galpão": "1",
+            "Sexo": "FEMEA",
+            "Peso previsto das aves": "2800",
+            "Aves por caixa": "8",
+            "Peso previsto por caixa": "15,577",
+            "Município": "Coimbra",
+            "Data": "16/11/2023",
+            "Horário de retirada da ração": "19:00",
+            "Horário": "02:00",
+            "Turma de apanha": "SINHORINHO",
+            "Idade": "47",
+            "Horário previsto chegada frigorífico": "03:40",
+            "Horário previsto início do abate": "06:40",
+            "Ordem dos caminhões p/ abate": "8",
+            "Placa": "XXXX-000",
+            "Nota fiscal": "FDIONU123",
+            "Horário de chegada": "03:40"
+        },
+        {
+            "Destino": "SIF - 926 (FABRIL)",
+            "Aves por veículo": "2850",
+            "Produtor": "JOSE VICENTE TOLEDO(POSITIVO)",
+            "Galpão": "1",
+            "Sexo": "FEMEA",
+            "Peso previsto das aves": "3840",
+            "Aves por caixa": "5",
+            "Peso previsto por caixa": "19,200",
+            "Município": "Ervália",
+            "Data": "15/11/2023",
+            "Horário de retirada da ração": "19:00",
+            "Horário": "22:00",
+            "Turma de apanha": "FÁBIO",
+            "Idade": "57",
+            "Horário previsto chegada frigorífico": "01:00",
+            "Horário previsto início do abate": "04:28",
+            "Ordem dos caminhões p/ abate": "3",
+            "Placa": "XXXX-000",
+            "Nota fiscal": "FDIONU123",
+            "Horário de chegada": "01:00"
+        },
+        {
+            "Destino": "SIF - 926 (FABRIL)",
+            "Aves por veículo": "2850",
+            "Produtor": "JOSE VICENTE TOLEDO(POSITIVO)",
+            "Galpão": "1",
+            "Sexo": "FEMEA",
+            "Peso previsto das aves": "3840",
+            "Aves por caixa": "5",
+            "Peso previsto por caixa": "19,200",
+            "Município": "Ervália",
+            "Data": "15/11/2023",
+            "Horário de retirada da ração": "19:00",
+            "Horário": "23:00",
+            "Turma de apanha": "FÁBIO",
+            "Idade": "57",
+            "Horário previsto chegada frigorífico": "02:00",
+            "Horário previsto início do abate": "05:38",
+            "Ordem dos caminhões p/ abate": "5",
+            "Placa": "XXXX-000",
+            "Nota fiscal": "FDIONU123",
+            "Horário de chegada": "02:00"
+        },
+        {
+            "Destino": "SIF - 926 (FABRIL)",
+            "Aves por veículo": "2850",
+            "Produtor": "JOSE VICENTE TOLEDO(POSITIVO)",
+            "Galpão": "1",
+            "Sexo": "FEMEA",
+            "Peso previsto das aves": "3840",
+            "Aves por caixa": "5",
+            "Peso previsto por caixa": "19,200",
+            "Município": "Ervália",
+            "Data": "16/11/2023",
+            "Horário de retirada da ração": "19:00",
+            "Horário": "00:00",
+            "Turma de apanha": "FÁBIO",
+            "Idade": "57",
+            "Horário previsto chegada frigorífico": "03:00",
+            "Horário previsto início do abate": "06:48",
+            "Ordem dos caminhões p/ abate": "7",
+            "Placa": "XXXX-000",
+            "Nota fiscal": "FDIONU123",
+            "Horário de chegada": "03:00"
+        },
+        {
+            "Destino": "SIF - 926 (FABRIL)",
+            "Aves por veículo": "2850",
+            "Produtor": "JOSE VICENTE TOLEDO(POSITIVO)",
+            "Galpão": "1",
+            "Sexo": "FEMEA",
+            "Peso previsto das aves": "3840",
+            "Aves por caixa": "5",
+            "Peso previsto por caixa": "19,200",
+            "Município": "Ervália",
+            "Data": "16/11/2023",
+            "Horário de retirada da ração": "19:00",
+            "Horário": "01:00",
+            "Turma de apanha": "FÁBIO",
+            "Idade": "57",
+            "Horário previsto chegada frigorífico": "04:00",
+            "Horário previsto início do abate": "07:58",
+            "Ordem dos caminhões p/ abate": "9",
+            "Placa": "XXXX-000",
+            "Nota fiscal": "FDIONU123",
+            "Horário de chegada": "04:00"
+        },
+        {
+            "Destino": "SIF - 926 (FABRIL)",
+            "Aves por veículo": "2850",
+            "Produtor": "JOSE VICENTE TOLEDO(POSITIVO)",
+            "Galpão": "1",
+            "Sexo": "FEMEA",
+            "Peso previsto das aves": "3840",
+            "Aves por caixa": "5",
+            "Peso previsto por caixa": "19,200",
+            "Município": "Ervália",
+            "Data": "16/11/2023",
+            "Horário de retirada da ração": "19:00",
+            "Horário": "02:00",
+            "Turma de apanha": "FÁBIO",
+            "Idade": "57",
+            "Horário previsto chegada frigorífico": "05:00",
+            "Horário previsto início do abate": "08:57",
+            "Ordem dos caminhões p/ abate": "11",
+            "Placa": "XXXX-000",
+            "Nota fiscal": "FDIONU123",
+            "Horário de chegada": "05:00"
+        }
+]
+
+Infos.forEach(element => {
+    const Keys = Object.keys(element);
+     new Caminhao(element[Keys[0]], element[Keys[1]], element[Keys[2]], element[Keys[3]], element[Keys[4]], element[Keys[5]], element[Keys[6]], element[Keys[7]], element[Keys[9]], element[Keys[10]], element[Keys[11]], element[Keys[12]], element[Keys[13]], element[Keys[14]], element[Keys[15]], element[Keys[16]], element[Keys[17]], element[Keys[18]], element[Keys[19]]);
+})
